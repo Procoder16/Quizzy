b9b555ea1f47f3ea3ebb5ed5750160288f27d508
Procoder16 <smsoumik1608@gmail.com>
2021-09-09 00:29:26 +0530

    added the buttons and the questions list

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,44 @@
 import 'package:flutter/material.dart';
 
-void main() {}
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
+  void answerQuestion() {
+    print('Answer chosen!');
+  }
+
   Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favourite color?',
+      'What\'s your favourite animal?',
+    ];
+
     return MaterialApp(
-      home: Text('Hello!'),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('QUIZZY!!!'),
+        ),
+        body: Column(
+          children: [
+            Text('Question 1'),
+            RaisedButton(
+              child: Text('Option 1'),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Option 2'),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Option 3'),
+              onPressed: answerQuestion,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
